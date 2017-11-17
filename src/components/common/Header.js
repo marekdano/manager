a2bf315e1d27dae83458d3cb2e5354a2879c4877
Marek Dano <mk.dano@gmail.com>
2017-11-17 23:52:54 +0000

    Add employee form with input fields.

---
+++ src/components/common/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Header = ({ headerText }) => (
+	<View style={styles.header}>
+		<Text>{headerText}</Text>
+	</View>
+);
+
+const styles = StyleSheet.create({
+  header: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative'
+	}	
+});
+
+export { Header };

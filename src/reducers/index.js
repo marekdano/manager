e5831395284a8311ce7110a7308ed5cc128b5f84
Marek Dano <mk.dano@gmail.com>
2017-11-19 20:03:55 +0000

    Link employee form with redux.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,6 @@ import { combineReducers } from 'redux';
 import EmployeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
-	employeeorm: EmployeeFormReducer,
+	employeeForm: EmployeeFormReducer,
 	sample: () => []
 });

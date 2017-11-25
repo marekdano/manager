be9d9c05288abe69a8f443a076d8b1313d7b88e8
Marek Dano <mk.dano@gmail.com>
2017-11-25 21:54:26 +0000

    Add handler for employee form.

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -6,5 +6,10 @@ export const employeeUpdate = ({ prop, value }) => {
 	return {
 		type: EMPLOYEE_UPDATE,
 		payload: { prop, value }
-	}
+	};
 };
+
+export const employeeCreate = ({ name, phone, shift }) => {
+	console.log(name, phone, shift);
+};
+

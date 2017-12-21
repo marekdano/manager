ae5448f8de976011d735d0007f91c3aa2887ba9c
Marek Dano <mk.dano@gmail.com>
2017-12-21 09:02:27 +0000

    Add firebase.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import Router from './Router';
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import firebase from "firebase";
+import reducers from "./reducers";
+import Router from "./Router";
 
 export default class App extends Component {
+  componentWillMount() {
+    // init firebase settings
+  }
+
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-    
+
     return (
       <Provider store={store}>
         <Router />
@@ -21,8 +26,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });

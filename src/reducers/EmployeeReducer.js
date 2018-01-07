504eb174e70e50bef6d725d9651cc078ad44a9a4
Marek Dano <mk.dano@gmail.com>
2018-01-07 14:52:00 +0100

    Storing data from firebase by ID as it is in firebase format.

--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -1,16 +1,13 @@
-import {
-	EMPLOYEES_FETCH_SUCCESS
-} from '../actions/types';
+import { EMPLOYEES_FETCH_SUCCESS } from "../actions/types";
 
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case EMPLOYEES_FETCH_SUCCESS: 
-			console.log(action);
-			//return action.payload;
-			return state;
-		default:
-			return state;
-	}
-}
+  switch (action.type) {
+    case EMPLOYEES_FETCH_SUCCESS:
+      //return { ...state, [id]: action.payload };
+      return action.payload;
+    default:
+      return state;
+  }
+};

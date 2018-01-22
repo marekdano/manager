3490ea601b5c77b59333607b44b078be5d44706b
Marek Dano <mk.dano@gmail.com>
2018-01-22 21:54:31 +0000

    Add Decline function to dismiss Modal.

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -32,6 +32,12 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  onAccept() {}
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -58,7 +64,11 @@ class EmployeeEdit extends Component {
           </Button>
         </CardSection>
 
-        <Confirm visible={this.state.showModal}>
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
           Are you sure you want to delete this?
         </Confirm>
       </Card>
